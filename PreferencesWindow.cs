07ee94abc8104239eb9dac445e1cef9c138aa6eb
Bryan Warren <brian@briansprojects.net>
2015-04-22 16:14:45 -0700

    added process to get input from lm-sensor

--- PreferencesWindow.cs
+++ PreferencesWindow.cs
@@ -1,11 +1,14 @@
 using Gtk;
 using System;
 using AppIndicator;
+using System.Diagnostics;
 
 namespace fglrxindicator
 {
 	public class PreferencesWindow
 	{
+		string _line;
+
 		public PreferencesWindow ()
 		{
 			var win = new Window ("Fglrx Indicator");
@@ -15,15 +18,37 @@ namespace fglrxindicator
 			BuildIndicator ();
 		}
 
+		private string ReadSensor()
+		{
+			Process proc = new Process 
+			{
+				StartInfo = new ProcessStartInfo 
+				{
+					FileName = "/usr/bin/sensors",
+					UseShellExecute = false,
+					RedirectStandardOutput = true
+				}
+			};
+
+			proc.Start();
+			while (!proc.StandardOutput.EndOfStream) 
+			{
+				_line = proc.StandardOutput.ReadToEnd();
+			}
+			proc.Close (); //Probably won't need this but doesn't hurt to have it
+			return _line;
+		}
+
 		private void BuildIndicator()
 		{
 			ApplicationIndicator indicator = new ApplicationIndicator
 			(
-				"Fglrx Indicator", 		//id of the the indicator icon
-				"catalyst",			        //file name of the icon (will look for app-icon.png) 
+				"Fglrx Indicator", //ID
+				"catalyst", //File Name
 				Category.Hardware,
-				System.IO.Path.GetDirectoryName (Environment.GetCommandLineArgs () [0])            //the folder where to look for app-icon.png
+				System.IO.Path.GetDirectoryName (Environment.GetCommandLineArgs () [0]) //TODO: Factor this so that it isn't so ugly
 			);
+			//TODO: Figure out what is needed and what is not
 			var menuItemQuit = new ImageMenuItem ("Quit");
 			menuItemQuit.Image = new Gtk.Image (Stock.Quit, IconSize.Menu);
 			menuItemQuit.Activated += (sender, e) => Application.Quit ();
@@ -34,7 +59,8 @@ namespace fglrxindicator
 			popupMenu.ShowAll();
 
 			indicator.Menu = popupMenu;
-			indicator.Status = AppIndicator.Status.Active;	
+			indicator.Status = AppIndicator.Status.Active;
+			ReadSensor (); //TODO: Put the output of this into the actual indicator
 		}
 	}
 }
